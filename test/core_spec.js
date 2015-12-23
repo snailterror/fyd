05a7538fd189fe50516e6a1da1c0031aca45bf89
Adrien <espel.adrien@gmail.com>
2015-12-23 11:22:14 +0100

    Base test

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,23 +1,52 @@
 import {expect} from 'chai';
 import jsdom from 'mocha-jsdom';
+import Fyd from '../lib/fyd';
 
-describe('application logic', () => {
+function fyd(f){
+    return new Fyd(f);
+}
 
-    describe('Test for tests :D', () => {
+describe('Class selector', () => {
 
-        jsdom();
-
-        it('Egal 2', () => {
-            const test = 2;
-            expect(test).to.equal(2);
-        });
-
-        it('return div', () => {
-            var div = document.createElement('div');
-            expect(div.nodeName).to.equal('DIV');
-        });
-
+    jsdom({
+        html : '<p class="doge">Just wow</p><p class="shibe">Holly ...</p><p class="shibe">Am p ! ...</p>'
     });
 
+    it('Has class doge', () => {
+        let div = fyd('p');
+        expect(div.hasClass('doge')).to.equal(true);
+    });
+
+    it('Set class much-wow', () => {
+        let div = fyd('p');
+        div.addClass('much-wow');
+        expect(div.hasClass('much-wow')).to.equal(true);
+    });
+
+    it('Remove class doge', () => {
+        let div = fyd('p');
+        div.removeClass('doge');
+        expect(div.hasClass('doge')).to.equal(false);
+    });
+
+    it('Return only div with class shibe', () => {
+        let div = fyd('p');
+        div.withClass('shibe');
+        expect(div.target.length).to.equal(2);
+    });
+
+});
+
+describe('Work with text', () => {
+
+    jsdom({
+        html : '<p class="doge">Just wow</p>'
+    });
+
+    it('Edit inner text', () => {
+        let div = fyd('p');
+        div.text('Hello Doge');
+        expect(div.text()).to.equal('Hello Doge');
+    });
 
 });
